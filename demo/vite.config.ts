720bbf0335f2bc78cff22eaf9c63f4f0670ad961
Javier Abia Presa <javiabia@endlessstud.io>
2025-10-13 23:39:24 +0200

    Update base

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/obradoiro-pablo-picasso-20251017/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
